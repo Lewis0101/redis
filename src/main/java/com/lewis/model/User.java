e9635bc160bb819697503de431b6923aa115f750
LiuYang <1005859278@qq.com>
2019-10-22 11:54:12 +0800

    Initial commit

---
+++ src/main/java/com/lewis/model/User.java
@@ -0,0 +1,61 @@
+package com.lewis.model;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.Serializable;
+
+import javax.annotation.Resource;
+
+/**
+ * @author : 00222 [liu.yang@unisinsight.com]
+ * @description:
+ * @date : 2019/10/22 11:17
+ * @since: 1.0
+ */
+public class User implements Serializable {
+    private static final long serialVersionUID = 1L;
+    /**
+     * 编号
+     */
+    private int id;
+    /**
+     * 姓名
+     */
+    private String name;
+    /**
+     * 年龄
+     */
+    private int age;
+
+    public User() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toString() {
+        return JSONObject.toJSONString(this);
+    }
+
+}
